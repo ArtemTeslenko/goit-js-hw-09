5e95f1462283582895e068ef7eacb04e8a79579d
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-20 01:02:03 +0300

    Second task
    Got dates, reacted on dates differences in 2 task.

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,51 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  start: document.querySelector('[data-start'),
+};
+let currentTime = null;
+let chosenTime = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const unitDate = new Date(selectedDates);
-    return unitDate.getTime();
+    chosenTime = new Date(selectedDates[0]).getTime();
+    currentTime = Date.now();
+    checkValidityOfDate(currentTime, chosenTime);
   },
 };
 
-const timer = Object.create(options);
-timer.runTime = true;
+const fp = flatpickr('#datetime-picker', options);
 
-console.log(timer.hasOwnProperty('enableTime'));
+function checkValidityOfDate(current, chosen) {
+  if (chosen <= current) {
+    window.alert('Please choose a date in the future');
+  } else {
+    refs.start.removeAttribute('disabled', 'disabled');
+  }
+}
 
-flatpickr('#datetime-picker', options);
+const timer = {
+  start() {
+    const startTime = Date.now();
+    // setInterval(() => {
+    //   const startTime = Date.now();
+    //   console.log(startTime);
+    // }, 1000);
+  },
+};
+
+timer.start();
+
+// -------------------------------------------TEST BTN
+const btnEl = document.querySelector('.test');
+btnEl.addEventListener('click', testFun);
+function testFun(e) {
+  console.log(chosenTime);
+}
+
+//Date.now() - возвращает текущее время
