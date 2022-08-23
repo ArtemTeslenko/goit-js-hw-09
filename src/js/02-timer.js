bbf8baa020a386241240dd70f7c2d7a0d1d419ff
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-23 11:11:40 +0300

    Fixed refs in first task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,11 +3,11 @@ import Notiflix from 'notiflix';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
-  start: document.querySelector('[data-start'),
-  days: document.querySelector('[data-days'),
-  hours: document.querySelector('[data-hours'),
-  minutes: document.querySelector('[data-minutes'),
-  seconds: document.querySelector('[data-seconds'),
+  start: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 let chosenTime = 0;
 const failureSettings = {
