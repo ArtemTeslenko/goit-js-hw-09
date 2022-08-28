276f7c5b7db5961f93ac5eeee8a8f4fb20f32812
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-28 14:52:59 +0300

    Finished 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   formEl: document.querySelector('.form'),
   firstDelay: document.querySelector('[name="delay"]'),
@@ -5,11 +7,21 @@ const refs = {
   amountDelay: document.querySelector('[name="amount"]'),
 };
 
+const alertSettings = {
+  clickToClose: true,
+  useIcon: false,
+  position: 'center-top',
+  width: '380px',
+  distance: '20px',
+  fontSize: '20px',
+  timeout: 3000,
+};
+
 refs.formEl.addEventListener('submit', function (e) {
   e.preventDefault();
-  showPromisResult(Number(refs.amountDelay.value));
+  const amountOfPromises = Number(refs.amountDelay.value);
+  showPromises(amountOfPromises);
 });
-// Number(refs.amountDelay.value)
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -22,27 +34,56 @@ function createPromise(position, delay) {
   });
 }
 
-// const am = 3;
-function showPromisResult(amount) {
-  const intervalId = setInterval(() => {
-    console.log(amount);
-    amount -= 1;
-    clearPromisesInterval(intervalId, amount);
-
-    createPromise(2, 1500)
+function showPromises(amount) {
+  let position = 1;
+  let delay = Number(refs.firstDelay.value);
+  let intervalId = setInterval(() => {
+    createPromise(position, delay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        successAlert(position, delay);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        failureAlurt(position, delay);
       });
-  }, 1000);
+
+    amount -= 1;
+    position += 1;
+
+    if (position === 2) {
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        createPromise(position, delay)
+          .then(({ position, delay }) => {
+            successAlert(position, delay);
+          })
+          .catch(({ position, delay }) => {
+            failureAlurt(position, delay);
+          });
+
+        amount -= 1;
+        position += 1;
+
+        delay += Number(refs.stepDelay.value);
+        if (amount === 0) {
+          clearInterval(intervalId);
+        }
+      }, Number(refs.stepDelay.value));
+    }
+
+    delay += Number(refs.stepDelay.value);
+  }, delay);
 }
 
-// showPromisResult(am);
+function successAlert(position, delay) {
+  Notiflix.Notify.success(
+    `✅ Fulfilled promise ${position} in ${delay}ms`,
+    alertSettings
+  );
+}
 
-function clearPromisesInterval(id, stopPoint) {
-  if (stopPoint < 1) {
-    clearInterval(id);
-  }
+function failureAlurt(position, delay) {
+  Notiflix.Notify.failure(
+    `❌ Rejected promise ${position} in ${delay}ms`,
+    alertSettings
+  );
 }
